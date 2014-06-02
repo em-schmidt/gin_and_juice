c9f04697e858287b06b75f99d63de5ac43a4dd2e
Eric M. Schmidt <eric@piratical.com>
2014-06-02 10:31:32 -0400

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GinAndJuice::Application.config.secret_key_base = '9d1b6a33898f4389b1b2356eb2c966e094b590ecdda787c2167940a335432b0137ea04b50fad02943c2c436c5124cfcc6598f7857bf26e8ca33cc6b0ff990e1c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GinAndJuice::Application.config.session_store :cookie_store, key: '_gin_and_juice_session'
