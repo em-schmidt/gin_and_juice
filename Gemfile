ded78a9de0f8aa8ed90a892eac773133d5e1dc4e
Eric M. Schmidt <eric@piratical.com>
2014-06-03 13:04:43 -0400

    secure token generation, and prep for test suite

--- Gemfile
+++ Gemfile
@@ -6,10 +6,17 @@ ruby '2.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.5'
 
-group :development do
+group :development, :test do
     # Use sqlite3 as the database for Active Record
     gem 'sqlite3', '1.3.8'
+    gem 'rspec-rails', '2.13.1'
 end
+
+group :test do
+	gem 'selenium-webdriver', '2.35.1'
+	gem 'capybara', '2.1.0'
+end
+	
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '4.0.1'
@@ -34,7 +41,7 @@ gem 'jbuilder', '1.0.2'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
-  gem 'sdoc', require: false
+  gem 'sdoc', '0.3.20', require: false
 end
 
 group :production do
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,21 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-GinAndJuice::Application.config.secret_key_base = '9d1b6a33898f4389b1b2356eb2c966e094b590ecdda787c2167940a335432b0137ea04b50fad02943c2c436c5124cfcc6598f7857bf26e8ca33cc6b0ff990e1c'
+
+require 'securerandom'
+
+def secure_token
+	token_file = Rails.root.join('.secret')
+	if File.exist?(token_file)
+		#Use the existing token
+		File.read(token_file).chomp
+	else
+		#Generate a new token and store it.
+		token = SecureRandom.hex(64)
+		File.write(token_file, token)
+		token
+	end
+end
+		
+
+GinAndJuice::Application.config.secret_key_base = secure_token
