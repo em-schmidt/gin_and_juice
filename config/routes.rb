79398e08f9de1c2076e52af3f3104b37db053157
Eric M. Schmidt <eric@piratical.com>
2014-06-03 13:34:21 -0400

    add static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 GinAndJuice::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
   resources :targets
 
   # The priority is based upon order of creation: first created -> highest priority.
