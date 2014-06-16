1118c5a826bcc5b107694db408bc3c147a156c23
Eric M. Schmidt <eric@piratical.com>
2014-06-16 10:16:37 -0400

    fix user email column to allow null

---
+++ db/migrate/20140616141449_change_users_email.rb
@@ -0,0 +1,5 @@
+class ChangeUsersEmail < ActiveRecord::Migration
+  def change
+      change_column :users, :email, :string, :null => true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140613001433) do
+ActiveRecord::Schema.define(version: 20140616141449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20140613001433) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                  default: ""
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
