26bb8f5fc2a0c8c7d8cf17fa6af4f0b439e16bde
Eric M. Schmidt <eric@piratical.com>
2014-06-19 17:46:13 -0400

    check stat cache in fs cralwer before enqueing to the s3 sender

--- app/workers/fs_crawler.rb
+++ app/workers/fs_crawler.rb
@@ -4,8 +4,22 @@ class FsCrawler
   
   def self.perform(fs_full_path, max_cache_age)
     Dir.chdir(fs_full_path) do 
-      Dir.glob("**/*").map { |path| Resque.enqueue(S3Sender, File.expand_path(path)) if File.file?(path) }
-    end
-  end
-
-end
+      Dir.glob("**/*").map do |path| 
+        if File.file?(path)
+          full_path = File.expand_path(path)
+          puts "checking path: #{full_path}"
+          cache_entry = STAT_CACHE.get(full_path)
+          if cache_entry.nil?
+            Resque.enqueue(S3Sender, full_path)
+          else
+            cache_create_time = JSON.parse(cache_entry)["cache_create_time"]
+            if (Time.now.to_i - cache_create_time) > max_cache_age
+              puts "now: #{Time.now.to_i} cache_age: #{Time.now.to_i - cache_create_time} max_cache_age: #{max_cache_age}"
+              Resque.enqueue(S3Sender, File.expand_path(path))
+            end 
+          end 
+        end 
+      end 
+    end 
+  end 
+end 
