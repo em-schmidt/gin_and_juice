4032038bfd41956e444d348df69b230751844d60
Eric M. Schmidt <eric@piratical.com>
2014-06-20 12:07:16 -0400

    log stat cache hits/skips

--- app/workers/fs_crawler.rb
+++ app/workers/fs_crawler.rb
@@ -10,13 +10,15 @@ class FsCrawler
           full_path = File.expand_path(path)
           cache_entry = STAT_CACHE.get(full_path)
           if cache_entry.nil?
-            Rails.logger.info "FsCrawler path: #{full_path} not in stat cache"
+            Rails.logger.info "FsCrawler path: #{full_path} not in stat cache, enqueueing"
             Resque.enqueue(S3Sender, full_path)
           else
             cache_create_time = JSON.parse(cache_entry)["cache_create_time"]
             if (Time.now.to_i - cache_create_time) > max_cache_age
-              Rails.logger.info "FsCrawler path: #{full_path} cache_age: #{Time.now.to_i - cache_create_time} max_cache_age: #{max_cache_age}"
+              Rails.logger.info "FsCrawler path: #{full_path} cache_age: #{Time.now.to_i - cache_create_time} max_cache_age: #{max_cache_age}, enqueueing"
               Resque.enqueue(S3Sender, File.expand_path(path))
+              else
+                Rails.logger.info "FsCrawler path: #{full_path} cache_age: #{Time.now.to_i - cache_create_time} max_cache_age: #{max_cache_age}, skipping"
             end 
           end 
         end 
