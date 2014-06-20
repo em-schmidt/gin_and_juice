73b8e908c7aaabc3191b07ad56bc118cbdb7b299
Eric M. Schmidt <eric@piratical.com>
2014-06-20 10:41:19 -0400

    logger should be Rails.logger

--- app/workers/fs_crawler.rb
+++ app/workers/fs_crawler.rb
@@ -3,19 +3,19 @@ class FsCrawler
   @queue = :fs_crawler_queue
   
   def self.perform(fs_full_path, max_cache_age)
-    logger.info "Starting crawl of path: #{fs_full_path}"
+    Rails.logger.info "Starting crawl of path: #{fs_full_path}"
     Dir.chdir(fs_full_path) do 
       Dir.glob("**/*").map do |path| 
         if File.file?(path)
           full_path = File.expand_path(path)
           cache_entry = STAT_CACHE.get(full_path)
           if cache_entry.nil?
-            logger.info "path: #{full_path} not in stat cache"
+            Rails.logger.info "path: #{full_path} not in stat cache"
             Resque.enqueue(S3Sender, full_path)
           else
             cache_create_time = JSON.parse(cache_entry)["cache_create_time"]
             if (Time.now.to_i - cache_create_time) > max_cache_age
-              logger.info "path: #{full_path} cache_age: #{Time.now.to_i - cache_create_time} max_cache_age: #{max_cache_age}"
+              Rails.logger.info "path: #{full_path} cache_age: #{Time.now.to_i - cache_create_time} max_cache_age: #{max_cache_age}"
               Resque.enqueue(S3Sender, File.expand_path(path))
             end 
           end 
