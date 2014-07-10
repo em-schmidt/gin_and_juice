c1353763e744e6f0569386aa43620b48f6a872a0
Eric M. Schmidt <eric@piratical.com>
2014-07-10 11:03:23 -0400

    Fix user email address population.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,16 @@ class User < ActiveRecord::Base
   devise :ldap_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
+  before_save :email_from_username
+         
   def email_required? 
     false
   end
+  
+  protected
+  
+  def email_from_username
+    self.email = "#{self.username}@parature.com"
+  end
+  
 end
