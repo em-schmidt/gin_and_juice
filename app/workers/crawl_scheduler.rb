3b2f1e79c57a2d71957f598f16a4b74ae59ed940
Eric M. Schmidt <eric@piratical.com>
2014-07-14 19:11:22 -0400

    remove useless login/session options

--- app/workers/crawl_scheduler.rb
+++ app/workers/crawl_scheduler.rb
@@ -1,12 +1,10 @@
 class CrawlScheduler
   @queue = :scheduler
-  
+
   def self.perform
     targets = Target.all
     targets.each do |t|
-      t.backup_now! if t.backup_due? 
+      t.backup_now! if t.backup_due?
     end
   end
 end
-
-
