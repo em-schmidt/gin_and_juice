030f8748d09a84e2d322839517d594c4e094c1bd
Eric M. Schmidt <eric@piratical.com>
2014-07-21 14:10:40 -0400

    adjust stage workers to prevent OOM Killer on stage VM

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -12,7 +12,7 @@ role :resque_worker, %w{deploy@stage-backup.local}
 role :resque_scheduler, %w{deploy@stage-backup.local}
 
 set :resque_environment_task, true
-set :workers, {"fs_crawler_queue" => 1, "s3_queue" => 9}
+set :workers, {"fs_crawler_queue" => 1, "s3_queue" => 4}
 
 set :rails_env, :staging
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
