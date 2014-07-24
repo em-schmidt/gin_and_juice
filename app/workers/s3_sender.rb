f10cf4b99b9c0c1a8e950f437f60762abdfa0f92
Eric M. Schmidt <eric@piratical.com>
2014-07-24 12:08:14 -0400

    add exception handling to s3 sender for missing files

--- app/workers/s3_sender.rb
+++ app/workers/s3_sender.rb
@@ -2,27 +2,31 @@ class S3Sender
 	@queue = :s3_queue
 	
 	def self.perform(file_path)
-	  
-	  f = File.open(file_path, 'r')	  
-	  digest = Digest::MD5.new()
-	  f.each_line { |line| digest << line }
-	  
-	  if f.size > 5242880
-      aws_file = S3.files.create(:key => file_path, :body => open(file_path), :encryption => 'AES256', :multipart_chunk_size => 5242880)
-	  else
-	    aws_file = S3.files.create(:key => file_path, :body => open(file_path), :encryption => 'AES256')
-	  end
-	  
-	  cache_entry = {cache_create_time: Time.now.to_i,
-	                 fs_create_time: f.ctime.to_i,
-  	               fs_size: f.size,
-  	               fs_md5: digest.to_s,
-  	               aws_etag: aws_file.etag,
-  	               aws_create_time: Time.now.to_i }
+        begin
+            f = File.open(file_path, 'r')	  
+            digest = Digest::MD5.new()
+            f.each_line { |line| digest << line }
 
-    STAT_CACHE[file_path] = cache_entry.to_json
+            if f.size > 52428800
+            aws_file = S3.files.create(:key => file_path, :body => open(file_path), :encryption => 'AES256', :multipart_chunk_size => 52428800)
+            else
+            aws_file = S3.files.create(:key => file_path, :body => open(file_path), :encryption => 'AES256')
+            end
 
-    Rails.logger.info "S3Sender sent #{file_path} md5: #{digest.to_s} etag: #{aws_file.etag}"
+            cache_entry = {cache_create_time: Time.now.to_i,
+                         fs_create_time: f.ctime.to_i,
+                       fs_size: f.size,
+                       fs_md5: digest.to_s,
+                       aws_etag: aws_file.etag,
+                       aws_create_time: Time.now.to_i }
+
+            STAT_CACHE[file_path] = cache_entry.to_json
+
+            Rails.logger.info "S3Sender sent #{file_path} md5: #{digest.to_s} etag: #{aws_file.etag}"
+        rescue Errno::ENOENT => e
+            Rails.logger.error "S3Sender: #{e.error}"
+        end
+
 
 	end
 
