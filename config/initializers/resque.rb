ca4ee87e880a05e11a11f1b984d74c1adddeaa5a
Eric M. Schmidt <eric@piratical.com>
2014-07-30 18:48:41 -0400

    workers reconnect to redis after fork

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,13 @@
 Resque::Scheduler.dynamic = true
 Resque.schedule = YAML.load_file(File.join('config/scheduler.yml'))
 
+Resque.before_fork do
+    defined?(ActiveRecord::Base) and
+        ActiveRecord::Base.connection.disconnect!
+end
+
+Resque.after_fork do
+    defined?(ActiveRecord::Base) and
+        ActiveRecord::Base.establish_connection
+    Resque.redis.client.reconnect
+end
