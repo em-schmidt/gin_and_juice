2af1625be67e11f72d95fcedcbd71281b518eae6
Eric M. Schmidt <eric@piratical.com>
2014-08-05 13:02:33 -0400

    fork work

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ GinAndJuice::Application.initialize!
 if defined?(PhusionPassenger)
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
     if forked
-      STAT_CACHE.redis.client.reconnect
+      stat_cache.redis.client.reconnect
       Resque.redis.client.reconnect
     end
   end
