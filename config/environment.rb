6aaab60b97922a30f6531650ed332196d407a8f4
Eric M. Schmidt <eric@piratical.com>
2014-08-05 13:15:12 -0400

    fork work

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 GinAndJuice::Application.initialize!
-
-if defined?(PhusionPassenger)
-  PhusionPassenger.on_event(:starting_worker_process) do |forked|
-    if forked
-      stat_cache.redis.client.reconnect
-      Resque.redis.client.reconnect
-    end
-  end
-end
