8e8f2839e0df0f018772ef7a45775ffdb1d3add1
Eric M. Schmidt <eric@piratical.com>
2014-08-05 12:54:58 -0400

    fork work

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,5 @@
 Resque::Scheduler.dynamic = false
-Resque.schedule = YAML.load_file(File.join('config/scheduler.yml'))
+#Resque.schedule = YAML.load_file(File.join('config/scheduler.yml'))
 
 Resque.after_fork do
     Resque.redis.client.reconnect
