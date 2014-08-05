95aaa032db268ad6d32e44b487283ae3b33faffd
Eric M. Schmidt <eric@piratical.com>
2014-08-05 10:59:02 -0400

    fixing inherited redis connections after fork

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 GinAndJuice::Application.initialize!
+
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    if forked
+      STAT_CACHE.redis.client.reconnect
+      end
+    end
+end
