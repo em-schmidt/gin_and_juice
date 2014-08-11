a0e00b4cf458ea837276c8d28ca022157642a6fa
Eric M. Schmidt <eric@piratical.com>
2014-08-11 15:03:45 -0400

    cache key work

--- app/workers/fs_crawler.rb
+++ app/workers/fs_crawler.rb
@@ -14,14 +14,17 @@ class FsCrawler
         full_path = File.expand_path(path)
         cache_entry = stat_cache.get(full_path)
         if cache_entry.nil?
+          # no cache entry... back this file up.
           Rails.logger.info "FsCrawler path: #{full_path} not in stat cache, enqueueing"
           Resque.enqueue(S3Sender, full_path)
         else
           cache_create_time = JSON.parse(cache_entry)["cache_create_time"]
           if (Time.now.to_i - cache_create_time) > max_cache_age
+            # cache entry is out of date... queue this puppy.
             Rails.logger.info "FsCrawler path: #{full_path} cache_age: #{Time.now.to_i - cache_create_time} max_cache_age: #{max_cache_age}, enqueueing"
             Resque.enqueue(S3Sender, File.expand_path(path))
             else
+              # TODO: cache entry is current, lets verify the other particulars match, fs_size, fs_create_time. 
               Rails.logger.info "FsCrawler path: #{full_path} cache_age: #{Time.now.to_i - cache_create_time} max_cache_age: #{max_cache_age}, skipping"
           end
         end
--- app/workers/s3_sender.rb
+++ app/workers/s3_sender.rb
@@ -6,10 +6,16 @@ class S3Sender
 	
 	def self.perform(file_path)
         begin
+            # TODO: check for cache_entry prior to pushing, if cache_entry exists, compare to s3 object prior to sending
+            stat_cache = Redis::Namespace.new(:statcache)
+            cache_entry = stat_cache.get(file_path)
+                        
             f = File.open(file_path, 'r')	  
             digest = Digest::MD5.new()
             f.each_line { |line| digest << line }
-            aws_key = file_path.gsub(/^\//, "")
+            
+            # if file name starts with an extra / remove it... should be old bug at this point 8/11
+            aws_key = file_path.gsub(/^\/\//, "/")
 
             if f.size > 52428800
             aws_file = S3.files.create(:key => aws_key, :body => open(file_path), :encryption => 'AES256', :multipart_chunk_size => 52428800)
@@ -17,7 +23,7 @@ class S3Sender
             aws_file = S3.files.create(:key => aws_key, :body => open(file_path), :encryption => 'AES256')
             end
 
-            cache_entry = {cache_create_time: Time.now.to_i,
+            new_cache_entry = {cache_create_time: Time.now.to_i,
                          fs_create_time: f.ctime.to_i,
                        fs_size: f.size,
                        fs_md5: digest.to_s,
@@ -25,9 +31,9 @@ class S3Sender
                        aws_create_time: Time.now.to_i }
 
             stat_cache = Redis::Namespace.new(:statcache)
-            stat_cache[file_path] = cache_entry.to_json
+            stat_cache[file_path] = new_cache_entry.to_json
 
-            Rails.logger.info "S3Sender sent #{file_path} md5: #{digest.to_s} etag: #{aws_file.etag}"
+            Rails.logger.info "S3Sender sent #{file_path} as #{aws_key} md5: #{digest.to_s} etag: #{aws_file.etag}"
         rescue Errno::ENOENT => e
             Rails.logger.info "S3Sender: #{e.message}"
         rescue Errno::EACCES => e
